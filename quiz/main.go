559a932a3892fa57fbc5f99350b96f4747152259
Peterson Salme <petersonsalme@gmail.com>
2020-09-14 17:06:12 -0300

    second part of quiz with refactoring

--- quiz/main.go
+++ quiz/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 type problem struct {
@@ -16,16 +17,63 @@ func (p problem) String() string {
 	return fmt.Sprintf("%s = ", p.question)
 }
 
-func main() {
-	csvFilename := flag.String("csv", "problems.csv", "CSV file with 'question,answer' format;")
-	flag.Parse()
+var (
+	csvFilename = flag.String("csv", "problems.csv", "CSV file with 'question,answer' format;")
+	timeLimit   = flag.Int("limit", 30, "limit time to solve the quiz")
+)
 
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	sliceOfProblems := loadProblems()
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	correct := 0
+
+problemLoop:
+	for index, problem := range sliceOfProblems {
+		fmt.Printf("Problem #%d: %s\n", index+1, problem)
+
+		answerChannel := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerChannel <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			showScore(correct, len(sliceOfProblems))
+			break problemLoop
+		case answer := <-answerChannel:
+			if answer == problem.answer {
+				correct++
+			}
+		}
+	}
+
+	showScore(correct, len(sliceOfProblems))
+}
+
+func loadProblems() []problem {
+	file := openFile()
+	lines := readFileLinesFrom(file)
+
+	return parseLinesIntoProblems(lines)
+}
+
+func openFile() (file *os.File) {
 	file, err := os.Open(*csvFilename)
+	defer file.Close()
 	if err != nil {
 		fmt.Printf("Failed to open file: %s", *csvFilename)
 		os.Exit(1)
 	}
+	return
+}
 
+func readFileLinesFrom(file *os.File) [][]string {
 	reader := csv.NewReader(file)
 	lines, err := reader.ReadAll()
 	if err != nil {
@@ -33,23 +81,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	correct := 0
-	for index, problem := range parseLines(lines) {
-		msg := fmt.Sprintf("Problem #%d: %s\n", index+1, problem)
-		fmt.Println(msg)
-
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-
-		if answer == problem.answer {
-			correct++
-		}
-	}
-
-	fmt.Printf("Scored %d out of %d\n", correct, len(lines))
+	return lines
 }
 
-func parseLines(lines [][]string) (result []problem) {
+func parseLinesIntoProblems(lines [][]string) (result []problem) {
 	result = make([]problem, len(lines))
 
 	for index, line := range lines {
@@ -60,4 +95,8 @@ func parseLines(lines [][]string) (result []problem) {
 	}
 
 	return
+}
+
+func showScore(correctAmount, problemAmount int) {
+	fmt.Printf("\nYou scored %d out of %d", correctAmount, problemAmount)
 }
